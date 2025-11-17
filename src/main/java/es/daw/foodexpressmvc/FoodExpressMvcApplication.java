fe3dd0d502b2a9d1a09d6c386fab326d08cc0bd3
profeMelola <melola@educa.madrid.org>
2025-11-17 17:02:26 +0100

    con contenido! perdóna

--- src/main/java/es/daw/foodexpressmvc/FoodExpressMvcApplication.java
+++ src/main/java/es/daw/foodexpressmvc/FoodExpressMvcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FoodExpressMvcApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FoodExpressMvcApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FoodExpressMvcApplication.class, args);
+    }
 
 }
--- src/main/java/es/daw/foodexpressmvc/repository/RoleRepository.java
+++ src/main/java/es/daw/foodexpressmvc/repository/RoleRepository.java
@@ -1,4 +1,11 @@
 package es.daw.foodexpressmvc.repository;
 
-public class RoleRepository {
+import es.daw.foodexpressmvc.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
 }
