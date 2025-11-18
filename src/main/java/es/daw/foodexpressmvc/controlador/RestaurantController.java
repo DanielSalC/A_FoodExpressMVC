7c882f9d067cb335c42c937ea024cea7d86a2e6a
DanielSalC <daniel.sc.4804@gmail.com>
2025-11-18 21:13:25 +0100

    Cambios realizados el 18 de Noviembre

--- src/main/java/es/daw/foodexpressmvc/controlador/RestaurantController.java
+++ src/main/java/es/daw/foodexpressmvc/controlador/RestaurantController.java
@@ -1,6 +1,8 @@
 package es.daw.foodexpressmvc.controlador;
 
 import es.daw.foodexpressmvc.dto.RestaurantDTO;
+import es.daw.foodexpressmvc.service.RestaurantService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +11,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+@RequiredArgsConstructor
 public class RestaurantController {
 
-    // Pendiente inyectar servicio
+
+    private final RestaurantService restaurantService;
 
     @GetMapping("/restaurants")
     public String listRestaurants(Model model) {
 
-        // pendiente obtener del servicio los restaurantes
-
-
-        List<RestaurantDTO> restaurants = new ArrayList<>();
-
+        List<RestaurantDTO> restaurants = restaurantService.getRestaurants();
         model.addAttribute("restaurants",restaurants);
         return "restaurants";
     }
--- src/main/java/es/daw/foodexpressmvc/service/RestaurantService.java
+++ src/main/java/es/daw/foodexpressmvc/service/RestaurantService.java
@@ -1,19 +1,34 @@
 package es.daw.foodexpressmvc.service;
 
 import es.daw.foodexpressmvc.dto.RestaurantDTO;
+import es.daw.foodexpressmvc.exception.ConnectApiRestException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class RestaurantService {
 
+    private final WebClient webClientAPI; //automaticamente hace la relación a public WebClient webClientAPI(WebClient.Builder builder)  en AppConfig.java
     public List<RestaurantDTO> getRestaurants() {
 
-        // Pendiente usar WebClient para hacer la petición al endpoint GET http://localhost:8082/api/restaurants
-        // Obtendremos un json la la lista de objetos RestaurantDTO
+        RestaurantDTO[] restaurants;
 
-        return null;
-
+        try {
+            restaurants = webClientAPI
+                    .get()
+                    .uri("/restaurants")
+                    .retrieve() //verifica el status. Si es es 4xx o 5xx, lanza error. Si es 2xx continua
+                    .bodyToMono(RestaurantDTO[].class)
+                    .block(); //Bloquea y espera. Sincrono
+        } catch (Exception e) {
+            //throw new ConnectApiRestException("Could not connect to FoodExpress API");
+            throw new ConnectApiRestException(e.getMessage());
+        }
+        return Arrays.asList(restaurants);
     }
 }
